64e5dd9c37a9b7d8c9e9c2db0bed41c7ec0742ee
Andrey <andreykushnir2021@gmail.com>
2025-12-10 18:49:45 +0200

    Fixed

--- MyString/MyString.cpp
+++ MyString/MyString.cpp
@@ -23,9 +23,38 @@ MyString::MyString(const char* input)
     strcpy(str, input);
 }
 
+MyString::MyString(const MyString& other): str(new char[other.length + 1]), length(other.length)
+{
+    strcpy(str, other.str);
+}
+
+
 MyString::~MyString()
 {
     delete[] str;
+}
+
+void MyString::setStr(const char* s)
+{
+    if (!s) {
+        str = new char[1];
+        str[0] = '\0';
+        length = 0;
+        return;
+    }
+    length = strlen(s);
+    str = new char[length + 1];
+    strcpy(str, s);
+}
+
+int MyString::Getlength() const
+{
+    return length;
+}
+
+const char* MyString::Getstr() const
+{
+    return str;
 }
 
 void MyString::input()
@@ -36,8 +65,9 @@ void MyString::input()
 
 void MyString::print() const
 {
-    cout << str << std::endl;
+    cout << str << endl;
 }
+
 
 void MyString::Mystrcpy(MyString& obj)
 {
@@ -67,6 +97,182 @@ int MyString::MyChar(char c)
     }
     return -1;
 }
+
+int MyString::Mystrlen()
+{
+    return length;
+}
+
+void MyString::MyStrCat(MyString& b)
+{
+    int newLength = length + b.length;
+    char* newStr = new char[newLength + 1];
+    for (int i = 0; i < length; i++)
+        newStr[i] = str[i];
+    for (int i = 0; i < b.length; i++)
+        newStr[length + i] = b.str[i];
+    newStr[newLength] = '\0';
+    delete[] str;
+    str = newStr;
+    length = newLength;
+}
+
+void MyString::MySteDel(char c)
+{
+    int count = 0;
+    for (int i = 0; i < length; i++)
+        if (str[i] == c)
+            count++;
+
+    if (count == 0)
+        return;
+
+    int newLength = length - count;
+    char* newStr = new char[newLength + 1];
+
+    int j = 0;
+
+    for (int i = 0; i < length; i++) {
+        if (str[i] != c)
+        {
+            newStr[j++] = str[i];
+        }
+        else continue;
+    }
+
+    newStr[newLength] = '\0';
+    delete[]str;
+    str = newStr;
+    length = newLength;
+}
+
+int MyString::MyStrCmp(MyString& b)
+{
+    int minLen = (length < b.length ? length : b.length);
+
+    for (int i = 0; i < minLen; i++)
+    {
+        if (str[i] < b.str[i])
+            return -1;
+
+        if (str[i] > b.str[i])
+            return 1;
+    }
+
+    if (length < b.length)
+        return -1;
+
+    if (length > b.length)
+        return 1;
+
+    return 0; 
+}
+
+char& MyString::operator[](int index)
+{
+    return str[index];
+}
+
+const char& MyString::operator[](int index) const
+{
+    return str[index];
+}
+
+MyString& MyString::operator=(MyString& b)
+{
+    if (this == &b) return *this;
+
+    delete[] str;
+
+    length = b.length;
+    str = new char[length + 1];
+
+    for (int i = 0; i < length; i++)
+        str[i] = b.str[i];
+
+    str[length] = '\0';
+
+    return *this;
+}
+
+MyString MyString::operator+(char c) const
+{
+    MyString temp;
+    temp.length = length + 1;
+    temp.str = new char[temp.length + 1];
+
+    for (int i = 0; i < length; i++) temp.str[i] = str[i];
+    temp.str[length] = c;
+    temp.str[temp.length] = '\0';
+
+    return temp;
+}
+
+MyString MyString::operator+(int n) const
+{
+    MyString temp;
+    temp.length = length + n;
+    temp.str = new char[temp.length + 1];
+
+    for (int i = 0; i < length; i++) temp.str[i] = str[i];
+    for (int i = 0; i < n; i++) temp.str[length + i] = '*';
+    temp.str[temp.length] = '\0';
+
+    return temp;
+}
+
+ostream& operator<<(std::ostream& out, const MyString& obj)
+{
+    out << obj.Getstr();
+    return out;
+}
+
+istream& operator>>(std::istream& in, MyString& obj)
+{
+    char buffer[1000];
+    in >> buffer;
+    obj.setStr(buffer);
+    return in;
+}
+
+MyString operator+(char c, const MyString& obj)
+{
+    MyString temp(obj.Getlength() + 1);  
+
+    char* buffer = new char[obj.Getlength() + 2]; 
+
+    buffer[0] = c; 
+
+    const char* s = obj.Getstr();
+    for (int i = 0; i < obj.Getlength(); i++)
+        buffer[i + 1] = s[i];
+
+   
+
+    temp.setStr(buffer); 
+
+    delete[] buffer;
+
+    return temp;
+}
+
+MyString operator+(int n, const MyString& obj)
+{
+    MyString temp;
+  
+    char* buffer = new char[obj.Getlength() + n + 1];
+    const char* s = obj.Getstr();
+    for (int i = 0; i < n; i++) buffer[i] = '*';
+    for (int i = 0; i < obj.Getlength(); i++) buffer[n + i] = s[i];
+    buffer[obj.Getlength() + n] = '\0';
+
+    temp.setStr(buffer);
+    delete[]buffer;
+    return temp;
+}
+
+
+
 
 
 
--- MyString/MyString.h
+++ MyString/MyString.h
@@ -3,18 +3,34 @@
 using namespace std;
 class MyString
 {
-	char* str; 
-	int length;
+    char* str;
+    int length;
 public:
     MyString();
     MyString(int n);
     MyString(const char* input);
+    MyString(const MyString& other);
     ~MyString();
+    void setStr(const char* s);
+    int Getlength() const;
+    const char* Getstr() const;
     void input();
     void print() const;
     void Mystrcpy(MyString& obj);
     bool Mystrstr(const char* str);
     int MyChar(char c);
-
+    int Mystrlen();
+    void MyStrCat(MyString& b);
+    void MySteDel(char c);
+    int MyStrCmp(MyString& b);
+    char& operator[](int index);
+    const char& operator[](int index) const;
+    MyString& operator=(MyString& b);
+    MyString operator+(char c) const;
+    MyString operator+(int n) const;
 };
 
+ostream& operator<<(std::ostream& out, const MyString& obj);
+istream& operator>>(std::istream& in, MyString& obj);
+MyString operator+(char c, const MyString& obj);
+MyString operator+(int n, const MyString& obj);
